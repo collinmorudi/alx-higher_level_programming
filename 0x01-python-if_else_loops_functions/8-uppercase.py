e1b975325fe150724caed425637cfaa65fd894fe
Collin Morudi <collinmorudi@gmail.com>
2022-11-29 18:23:04 +0200

    [Feature] a function that prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+    for ch in str:
+        if 97 <= ord(ch) <= 122:
+            x = ord(ch) - 32
+            upper += chr(x)
+            continue
+        upper += ch
+    print(upper)
