1a5d67695af8108e688ccf69725cfb8b0460cdf1
Collin Morudi <collinmorudi@gmail.com>
2022-12-01 22:40:13 +0200

    initial commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    x = int(sys.argv[1])
+    y = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(x, y, add(x, y)))
+    elif op == '-':
+        print("{} - {} = {}".format(x, y, sub(x, y)))
+    elif op == '*':
+        print("{} * {} = {}".format(x, y, mul(x, y)))i
+    else:
+        print("{} / {} = {}".format(x, y, div(x, y)))
