45e0869c729a2e37c005d0a776f3021d4a03e99a
Collin Morudi <collinmorudi@gmail.com>
2022-12-01 22:24:37 +0200

    initial commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    if (len(sys.argv) > 1):
-        for i in range(1, len(sys.argv)):
-            result += (int(sys.argv[i]))
-    print("{:d}".format(result))
+    from sys import argv
+    user_input = argv[1:]
+    size = len(user_input)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) is not 1 else "argument",
+                 "." if (size) is 0 else ":"))
+    for idx, arg in enumerate(user_input):
+        print("{:d}: {:s}".format(idx + 1, arg))
