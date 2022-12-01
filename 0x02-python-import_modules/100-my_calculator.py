42d1920588450c834cd70478a5437b5530a64499
Collin Morudi <collinmorudi@gmail.com>
2022-12-01 23:22:09 +0200

    fixing errors in scripts

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
         sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
-    x = int(sys.argv[1])
-    y = int(sys.argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
     if op == '+':
-        print("{} + {} = {}".format(x, y, add(x, y)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == '-':
-        print("{} - {} = {}".format(x, y, sub(x, y)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == '*':
-        print("{} * {} = {}".format(x, y, mul(x, y)))i
+        print("{} * {} = {}".format(a, b, mul(a, b)))i
     else:
-        print("{} / {} = {}".format(x, y, div(x, y)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
