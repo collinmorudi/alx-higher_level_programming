e9d2962543b9adc7f31f0ed370269009b5035199
Collin Morudi <collinmorudi@gmail.com>
2022-12-19 21:09:20 +0200

    Fixing the code according to pycodestyle guide

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         answer = a / b
-    except:
+    except (ZeroDivisionError, TypeError):
         answer = None
     finally:
         print("Inside result: {}".format(answer))
