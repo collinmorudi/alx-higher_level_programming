077e15dc0777f01b70d9dfac72e1efdf3786427c
Collin Morudi <collinmorudi@gmail.com>
2022-12-21 06:33:59 +0200

    documenting the class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+File 3-square
+Defines class Square with private attribute size and public attribute area
+"""
+
+
 class Square:
     """ A class that defines a square by its side size
     """
