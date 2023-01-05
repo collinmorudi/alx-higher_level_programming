ebd99f6443917f6bc69c65248740d2cd64ee840c
Collin Morudi <collinmorudi@gmail.com>
2023-01-05 05:02:17 +0200

    fixing errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,4 @@ allow dynmaically created attributes
 
 
 class LockedClass:
-    __slots__ = ['first_name']
-
-    def __init__(self):
-        """ Init method """
-        pass
+    __slots__ = 'first_name'
