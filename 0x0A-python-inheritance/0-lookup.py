07514a00907d77f001060300d3483b9e04783d50
Collin Morudi <collinmorudi@gmail.com>
2023-01-10 05:45:53 +0200

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup - Contains the lookup method that returns
+list of object's attribute and methods
+"""
+
+
+def lookup(obj):
+    """returns the list of object's attribute and methods"""
+    return dir(obj)
