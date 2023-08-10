a0e4e0e32244d69c6b8718c80ed3b8921f5c8079
Collin Morudi <collinmorudi@gmail.com>
2023-08-10 14:16:59 +0200

    adding 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+def islower(c):
+    return 97 <= ord(c) <= 122
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    new_str = ""
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            new_str += chr(ord(c) - 32)
+        else:
+            new_str += c
+    print(new_str)
