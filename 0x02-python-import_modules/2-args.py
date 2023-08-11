7c0ad0c7b3b3c9b70c7e10d1d07dbd9a075a3fab
Collin Morudi <collinmorudi@gmail.com>
2023-08-11 14:58:40 +0200

    adding 1-calculation.py file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+size = len(argv) - 1
+
+if size > 0:
+    print("{} arguments:".format(size))
+
+    for i in range(1, size + 1):
+        print("{}: {}".format(i, argv[i]))
+else:
+    print("{} arguments.".format(size))
