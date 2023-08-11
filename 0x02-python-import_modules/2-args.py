7b5ec8a40c27ab83a986971333016d27acd2c5e5
Collin Morudi <collinmorudi@gmail.com>
2023-08-11 15:11:29 +0200

    adding 3-infinite_add.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 
 from sys import argv
 
-size = len(argv) - 1
+if __name__ == "__main__":
+    size = len(argv) - 1
 
-if size > 0:
-    print("{} arguments:".format(size))
+    if size > 0:
+        print("{} arguments:".format(size))
 
-    for i in range(1, size + 1):
-        print("{}: {}".format(i, argv[i]))
-else:
-    print("{} arguments.".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, argv[i]))
+    else:
+        print("{} arguments.".format(size))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    size = len(argv) - 1
+    total = 0
+    
+    for i in range(1, size + 1):
+        total += int(argv[i])
+
+    print("{}".format(total))
