cb7cfa35c3f137efb582e70f6fce125a310e4f0f
Collin Morudi <collinmorudi@gmail.com>
2023-08-12 12:25:46 +0200

    4-new_in_list.py file

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx in range(len(my_list)):
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+
+    return my_list
