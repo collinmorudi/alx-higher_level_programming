79559433d737e0d3894f514c859186c35e16c0a9
Collin Morudi <collinmorudi@gmail.com>
2023-08-12 05:19:46 +0200

    fixing bugs in 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ from sys import argv
 if __name__ == "__main__":
     size = len(argv) - 1
 
-    if size > 0:
+    if size == 1:
+        print("{} argument:".format(size))
+        print("{}: {}".format(1, argv[1]))
+    elif size > 1:
         print("{} arguments:".format(size))
 
         for i in range(1, size + 1):
