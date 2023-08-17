e0ee508bfbcc1595b814682dcf40957b0fdcf53f
Collin Morudi <collinmorudi@gmail.com>
2023-08-17 06:08:27 +0200

    initial commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            new_matrix[row][col] = matrix[row][col] ** 2
-
-    return new_matrix
+    if matrix is not None:
+        new = []
+        for rows in matrix:
+            new.append(list(map(lambda x: x**2, rows)))
+        return (new)
+    return None
