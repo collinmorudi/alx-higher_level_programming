dd8836ae296c162a9b827b4b03140981e30f740a
Collin Morudi <collinmorudi@gmail.com>
2024-07-31 06:00:15 +0200

    b

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+
+const url = 'http://swapi.co/api/films/' + id;
+
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    for (const i in body.characters) {
+      request(body.characters[i], function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
+});
